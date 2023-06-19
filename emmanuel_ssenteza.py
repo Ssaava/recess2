95a5d60a1dc4f76b4707bc2de586f35f4c0cf895
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-06-19 12:02:11 +0300

    This is the first lesson waiting for the 2nd one

---
+++ emmanuel_ssenteza.py
@@ -0,0 +1,27 @@
+#Running Python Scripts
+print("Running Python Scripts")
+print("I love Python Scripts")
+#PEP8 Script guidelines
+#indentation is key in python programming
+#use CamelCase, snake_case, pascalcase to declare function names, variables
+'''
+This is how we dey do multiline comments in Python
+'''
+#variables 
+name = "Ssenteza Emmanuel"
+age = 14
+print(name , age)
+#Data structures
+'''
+Strings , integers, floats, 
+booleans  - [true, false], 
+sequences - [
+    lists - enclosed with square brackets [], 
+    tuple - enclosed with parentheses (),
+    range - use iterations, loops, etc
+    ], 
+Mapping types - 
+    dict - enclosing with carry braces {},
+'''
+gender_is = True
+print(gender_is)
