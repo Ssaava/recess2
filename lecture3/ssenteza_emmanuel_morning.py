e40c92b852cd37431c3f92cb1063bd96ffcbc5c8
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-06-23 12:54:12 +0300

    This is the first lesson waiting for the 2nd one

---
+++ lecture3/ssenteza_emmanuel_morning.py
@@ -0,0 +1,153 @@
+'''
+Operators and Expressions in python
+
+Arithmetic operators
+    + Addition
+    - Subtraction
+    * multiplication
+    / Division
+    % Modulus
+    **Exponentiation
+
+Comparison Operators
+    ==Equal to
+    != Not Equal !==
+    >Greater than
+    < Less than
+    >= Greater than or equal to
+    <= Less than or equal to
+
+Logical Operators
+    Logical AND 'and'
+    Logical Not 'not'
+
+Assignment Operators
+    Assign value:'+='
+    Subtract and assign value:'-='
+    Multiply and assign value:'*='
+    Divide and assign value:'/='
+    Modulus and assign value:'%='
+    Exponentiate and assign value:'**='
+
+Membership Operators:
+    In:'In' operator checks if a value exists in a sequence
+    Not in:'not in' operator checks if a value does not exist in a sequence
+
+Identity operators
+    Is:'is' operator:checks if two values are the same
+    Is not: 'is not' operator checks if two values are not the same
+
+'''
+# Examples
+# Addition
+x = 20 + 10
+print(x)
+
+# subtraction
+b = 4 - 2
+print(b)
+
+# Division
+y = 20 / 2
+print(y)
+
+# Divide
+z = 20 // 2
+print(z)
+
+# modulus
+d = 20 % 2
+print(d)
+
+# Multiplication
+e = 2 * 3
+print(e)
+
+# Exponentiate
+f = 2 ** 4
+print(f)
+
+a = 2
+c = 4
+# Equal to
+if a == b:
+    print("a is equal to b")
+    print(a == b)
+
+# Not equal to
+if a != b:
+    print("a is not equal to b")
+    print(a != b)
+
+# Greater than
+if a > b:
+    print("a is greater than b")
+    print(a > b)
+
+# Less than
+if a < b:
+    print("a is less than b")
+    print(a < b)
+
+# Greater than or equal to
+if a >= b:
+    print("a is greater than or equal to b")
+    print(a >= b)
+
+# Less than or equal to
+if a <= b:
+    print("a is less than or equal to b")
+    print(a <= b)
+
+# Logical operators
+a = True
+b = False
+
+# logical AND
+print(a and b)
+
+# logical OR
+print(a or b)
+
+# logical NOT
+print(not a)
+print(not b)
+
+# Assignment operators
+a = 19
+# compound assignment operator
+
+a += 5
+print(a)
+
+# Subtract and assign value
+a -= 9
+print(a)
+
+# membered operator
+cars = ['Jeep', 'Tesla', "BMW", 'RollRoyce']
+if 'jeep' in cars:
+    print("Jeep is in the list")
+
+
+print("This is my calculator app using GUI")
+'''
+operations needed to be carried out
+Addtion
+Subtraction
+Multiplication
+Division
+
+Buttons needed
+operation buttons 
+deletion buttons 
+operand buttons from 1-9
+
+functions needed 
+equals function
+operand function
+operator function
+clear function
+delete function
+
+'''
