8869af34450297c9ca2327295e1d3e399922d580
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-07-24 11:34:44 +0300

    lecture done

--- django/EmmaDjango/index.py
+++ django/EmmaDjango/index.py
@@ -2,12 +2,23 @@
 
 '''
 Steps:
-Set up the virtual environment (pip3 install pipenv)
-Install the extension (python intellisense)
+# Set up the virtual environment (pip3 install pipenv)
 
-Install django (pipenv install django)
+# Install the extension (python intellisense)
 
-Activate virtual environment (pipenv shell)
+# Install django (pipenv install django)
 
-Start new django project (django-admin startproject myapp)
+# Activate virtual environment (pipenv shell)
+
+# Start new django project (django-admin startproject myapp)
+
+# Run server (python manage.py runserver) 
+    this will use the default port 8000
+    to specify port, you can create one using (python manage.py runserver 9000)
+
+# Create a virtual environment for vscode to automatically activate the virtual environment
+ pipenv --venv (to show the virtual environment)
+
+# Intergrate terminal in vsCode 
+C:\Users\EMMA\.virtualenvs\EmmaDjango-h6lDchJF\bin\python
 '''
