ae22401360e12bf3b3219ebfd9acad8517a5d513
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-07-24 10:30:04 +0300

    Working with Django

---
+++ django/EmmaDjango/index.py
@@ -0,0 +1,13 @@
+# Getting started with Django
+
+'''
+Steps:
+Set up the virtual environment (pip3 install pipenv)
+Install the extension (python intellisense)
+
+Install django (pipenv install django)
+
+Activate virtual environment (pipenv shell)
+
+Start new django project (django-admin startproject myapp)
+'''
